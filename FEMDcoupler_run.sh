8af3b134bf02e08077504742bfbcb52f9ffd9c50
Lukas Laubert <lukas.laubert@fau.de>
2026-02-11 00:33:55 +0100

    v1.3.3
    - Added `skip_abaqus_GUI` parameter to `FEMDcoupler_params.py` and updated `FEMDcoupler_run.bat` and `FEMDcoupler_run.sh` to toggle between `noGUI` and `script` (GUI) mode for Abaqus, redirecting Abaqus output to the terminal when noGUI is used.
    - Updated section **Common Linux Issues** in `README.md` to reflect that Headless Mode is now controlled via the `skip_abaqus_GUI` parameter instead of manual script modification.

--- FEMDcoupler_run.sh
+++ FEMDcoupler_run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 JSON_FILE=$(python3 FEMDcoupler_params.py)
+ABAQUS_MODE=$(python3 -c "import json; print('noGUI' if json.load(open('$JSON_FILE'))['skip_abaqus_GUI'] else 'script')")
 x-terminal-emulator -e "bash -c 'python3 create_anchors_cut_notch.py \"$JSON_FILE\"'" &
-abaqus cae script=create_part_mesh.py -- "$JSON_FILE"
+abaqus cae $ABAQUS_MODE=create_part_mesh.py -- "$JSON_FILE"
 python3 create_periodicity_mesh.py "$JSON_FILE"
-python3 create_merge_FEMD_data_inp.py "JSON_FILE"
+python3 create_merge_FEMD_data_inp.py "$JSON_FILE"
 
 read -p "Press Enter to exit."
